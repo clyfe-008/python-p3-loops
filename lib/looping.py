d96d1fcef47788cc74446017f68250795ba7b5d6
Ruan <cliff.kamau@student.moringaschool.com>
2023-08-19 09:03:35 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+     
+    counter =10
+    while counter>0:
+        print(counter)
+        counter -=1
+        print("Happy New Year!")
+# code goes here!
+     pass
+
 
 def square_integers(int_list):
+    return list(map(lambda num: num ** 2, int_list))
     # code goes here!
     pass
 
 def fizzbuzz():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
     # code goes here!
     pass
